3893f4adeb51ac07a485407132640f7276958393
bernardoalvesmelo <bernardoalvesdemelo123@gmail.com>
2023-04-09 10:26:59 -0300

    Add project files.

---
+++ ContaBancaria/Movimentacao.cs
@@ -0,0 +1,29 @@
+﻿namespace ContaBancaria
+{
+    public class Movimentacao
+    {
+        private decimal valor;
+        private string tipo;
+        public decimal Valor
+        {
+            get
+            {
+                return this.valor;
+            }
+        }
+
+        public string Tipo
+        {
+            get
+            {
+                return this.tipo;
+            }
+        }
+
+        public Movimentacao(decimal valor, string tipo)
+        {
+            this.valor = valor;
+            this.tipo = tipo;
+        }
+    }
+}
---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ContaBancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta contaComum = new Conta(230, 100, false, 100);
+            Conta contaEspecial = new Conta(231, 100, true, 100);
+            contaComum.RealizarTransferencia(contaEspecial, 50);
+            Console.WriteLine(contaComum.RealizarSaque(150));
+            Console.WriteLine(contaEspecial.RealizarDeposito(50));
+            Console.WriteLine(contaComum.ExibirExtrato());
+            Console.WriteLine(contaEspecial.ExibirExtrato());
+            Console.ReadLine();
+        }
+    }
+}
